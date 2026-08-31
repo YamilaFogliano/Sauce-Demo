c9b6516ed624a7d09c2b88f3c7e8b21bbf69eb2d
YamilaFogliano <yamilafogliano@hotmail.com>
2026-08-31 10:03:24 -0300

    Mejoras en configuración

---
+++ README.md
@@ -0,0 +1,13 @@
+SauceDemo es una aplicación web diseñada para probar funcionalidades relacionadas con autenticación, navegación y flujos de compra en un comercio electrónico. Su propósito es simular casos de prueba comunes en entornos de desarrollo.
+
+- Características principales de SauceDemo:
+
+✔ Usuarios de prueba: Ofrece credenciales predefinidas para probar diferentes comportamientos y restricciones de acceso. Cuenta con un sistema de autenticación con diferentes tipos de usuarios.
+
+✔ Simulación de comercio electrónico: Permite navegar por un catálogo, agregar productos al carrito, filtrar artículos y completar un proceso de pago. Simula un flujo de compra desde la selección de productos hasta el checkout. Incluye la creación de un conprobante de pago que se descarga automaticamente en formato PDF. 
+
+✔ Entorno controlado: Contiene errores intencionales en algunos usuarios o elementos para practicar la detección de fallos.
+
+
+
+
---
+++ playwright.config.ts
@@ -0,0 +1,126 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+import dotenv from 'dotenv';
+import path from 'node:path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  // Aumentar un poco el timeout global en CI por la latencia del sitio
+  timeout: process.env.CI ? 60000 : 30000,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    ['html'], // Mantiene el reporte HTML nativo de Playwright
+    ['list'], // Muestra progreso en consola
+    [
+      'allure-playwright',
+      {
+        detail: true,
+        outputFolder: 'allure-results', // Carpeta donde se guardarán los JSON e imágenes
+        suiteTitle: false,
+      },
+    ],
+  ],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    headless: true,
+    /* Base URL to use in actions like `await page.goto('')`. */
+    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://opensource-demo.orangehrmlive.com',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry', //Graba video durante toda la prueba, pero solo guarda el archivo si el test falla
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure', // Capturas automáticas cuando falle un test
+    //launchOptions: {slowMo: 1000}, // Para Slow Motion
+  },
+
+
+  /* Configure projects for major browsers */
+  projects: [
+
+    //Config de Auth
+    {
+      name: 'setup',
+      testMatch: '**/*.setup.ts',
+    },
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        storageState: '.auth/admin.json',
+      },
+      dependencies: ['setup'],
+      testIgnore: '**/*.api.spec.ts',
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+        storageState: '.auth/admin.json',
+      },
+      dependencies: ['setup'],
+      testIgnore: '**/*.api.spec.ts',
+    },
+
+    {
+      name: 'api',
+      testMatch: '**/*.api.spec.ts',
+      use: {
+        video: 'off',
+        screenshot: 'off',
+        extraHTTPHeaders: {
+          'Accept': 'application/json',
+        },
+      },
+      dependencies: ['setup'],
+    },
+
+    /*     {
+          name: 'webkit',
+          use: { ...devices['Desktop Safari'] },
+        },
+     */
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
